74cb901f9abce50f0850bfd0a05b75ea8e27ce9e
peppeplanet <online@funridersindia.com>
2026-01-09 17:27:39 +0530

    2nd commit

--- funriders/core/urls.py
+++ funriders/core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('career/', views.career, name='career'),
+    path('about/', views.about, name='about'),
 ]
--- funriders/core/views.py
+++ funriders/core/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def contact(request):
+    print("Request method:", request.method)
+
+    if request.method == "POST":
+        print("POST data received:", request.POST)
+
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            print("Form is valid")
+            print("Cleaned data:", form.cleaned_data)
+
+            form.save()
+            print("Form saved to database")
+
+            messages.success(request, "Thank you! We will contact you shortly.")
+            return redirect('contact')
+        else:
+            print("Form is NOT valid")
+            print("Form errors:", form.errors)
+
+    else:
+        print("GET request – loading empty form")
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
+
+def career(request):
+    return render(request, "career.html")
+
+def about(request):
+    return render(request, "about.html")
+
+
